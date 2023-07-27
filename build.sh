44f7f37ddd133ea40fea72e345ea200075282a71
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2023-07-27 17:28:32 +0200

    Increase build script reliability

--- build.sh
+++ build.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
+scriptlocation=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+[ "$scriptlocation" = "$PWD" ] || (echo "Current directory isn't the same as the script's loctaion, exiting to avoid clobbering other files" && exit 1)
+
+pandoc_args="$*"
 
 build() {
   filename="${1%.*}"
-  pandoc --standalone --toc=true --template=template.html "$1" -o "out/$filename.html"
+  pandoc --standalone --toc=true --template=template.html $pandoc_args "$1" -o "out/$filename.html"
 }
 
+rm -r out/
 cp -r --reflink=auto assets out
 
 build home.md
+
+echo Build complete
 
